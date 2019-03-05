7d111f8e43b07031a165189675dc65473bf38400
Alex <reichling.alex@gmail.com>
2019-03-05 15:40:26 -0500

    Adding JS file

---
+++ JavaScript.js
@@ -0,0 +1,8 @@
+function highlightIcon(iconName){
+    document.getElementById("homeIcon").classList.remove('highlighted');
+    document.getElementById("aboutIcon").classList.remove('highlighted');
+    document.getElementById("resumeIcon").classList.remove('highlighted');
+    document.getElementById("COOPIcon").classList.remove('highlighted');
+    document.getElementById("projectsIcon").classList.remove('highlighted');
+    document.getElementById(iconName).classList.add('highlighted');
+}
