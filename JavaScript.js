553b0003073be64aca4b01d1219cc69f895166a7
Alex <reichling.alex@gmail.com>
2019-03-11 17:13:43 -0400

    adding HTML, CSS, JS changes and more imgs

--- JavaScript.js
+++ JavaScript.js
@@ -11,3 +11,15 @@ function onLoad(){
     window.scrollTo(0,0);
     document.getElementById("homeIcon").classList.add('highlighted');
 }
+
+window.addEventListener ("scroll", function(ev){
+    const pages = document.getElementsByClassName("Page");
+    var offsets = new Array(pages.length);
+
+    for (let i = 0; i < pages.length; i++)
+    {
+        console.log(pages[i].offsetHeight);
+    }
+    console.log("_______________________________________________________________________________________________");
+}
+)
