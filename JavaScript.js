f78da8af39bee0bbad3f3937f1f2459edf5605e7
Alex <reichling.alex@gmail.com>
2019-04-14 20:59:57 -0400

    Final Layout

--- JavaScript.js
+++ JavaScript.js
@@ -2,7 +2,6 @@ function highlightIcon(iconName){
     document.getElementById("homeIcon").classList.remove('highlighted');
     document.getElementById("aboutIcon").classList.remove('highlighted');
     document.getElementById("resumeIcon").classList.remove('highlighted');
-    document.getElementById("COOPIcon").classList.remove('highlighted');
     document.getElementById("projectsIcon").classList.remove('highlighted');
     document.getElementById(iconName).classList.add('highlighted');
 }
